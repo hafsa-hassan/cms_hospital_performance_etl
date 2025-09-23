b8f95a38e07343eb8da91c09faf8a40fed9e89ad
Hafsa Hassan <hafsafarah900@gmail.com>
2025-09-23 18:54:32 -0400

    transform_mspb_data

--- scripts/extract_data.py
+++ scripts/extract_data.py
@@ -1,11 +1,22 @@
 import requests
 import pandas as pd
 
-url = f"https://data.cms.gov/provider-data/api/1/datastore/query/xubh-q36u/0"
+def fetch_apis(url, filename):
 
-response = requests.get(url)
-data = response.json()
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
 
-df = pd.json_normalize(data["results"])
+    df = pd.json_normalize(data["results"])
+    df.to_csv(filename, index=False)
 
-df.to_csv("data/raw/raw_data.csv", index=False)
+    return df
+
+apis = {
+    "data/raw/raw_data.csv": "https://data.cms.gov/provider-data/api/1/datastore/query/xubh-q36u/0",
+    "data/raw/cost_raw_data.csv": "https://data.cms.gov/provider-data/api/1/datastore/query/rrqw-56er/0"
+
+}
+
+for filename, url in apis.items():
+    fetch_apis(url, filename)
